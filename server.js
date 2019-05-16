c86c4369e07b2d5041c24bb5103576e68dcb1117
Daniel Homann <danielhomann4@gmail.com>
2019-05-16 10:24:12 -0700

    got tables created in mySql

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var express = require("express");
 
 var app = express();
+var PORT = process.env.PORT || 8080;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+var db = require("./Models");
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 require("./Routes/htmlRoutes")(app);
 require("./Routes/apiRoutes")(app);
+require("./Routes/post-api-routes.js")(app);
 
 // listen on port 3000
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
